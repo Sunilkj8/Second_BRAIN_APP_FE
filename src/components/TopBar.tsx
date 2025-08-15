81e15d5e7ce52fa6b198587e5d297703596ad987
Sunilkj8 <109097652+Sunilkj8@users.noreply.github.com>
2025-08-15 22:57:18 +0530

    Update TopBar.tsx

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,6 +3,7 @@ import PlusIcon from "../icons/plusIcon";
 import ShareIcon from "../icons/shareIcon";
 import ButtonComp from "./Button";
 import AddContent from "./AddContent";
+import axios from "axios";
 
 const TopBar = () => {
   return (
@@ -11,10 +12,42 @@ const TopBar = () => {
 
       <div className="flex px-5 gap-2 ">
         <ButtonComp
-          text="Share"
+          text={
+            localStorage.getItem("shareableLink") ? "Unshare" : "Share"
+          }
           variant="secondary"
-          OnClick={() => {
-            console.log("clicked");
+          OnClick={async () => {
+            if (!localStorage.getItem("shareableLink")) {
+              try {
+                const res = await axios.post(
+                  "http://localhost:3000/api/v1/brain/share",
+                  {
+                    share: true,
+                  },
+                  {
+                    headers: { authorization: localStorage.getItem("token") },
+                  }
+                );
+
+                console.log(res.data.Share);
+                localStorage.setItem("shareableLink", res.data.Share);
+              } catch (e) {
+                console.log("Brain is already Shared");
+              }
+            } else {
+              try {
+                const res = await axios.post(
+                  "http://localhost:3000/api/v1/brain/share",
+                  { share: false },
+                  { headers: { authorization: localStorage.getItem("token") } }
+                );
+                localStorage.removeItem("shareableLink");
+                console.log(res.data);
+              } catch (error) {
+                console.log(error);
+              }
+            }
+            location.reload();
           }}
           size="md"
           startIcon={<ShareIcon size="md" />}
